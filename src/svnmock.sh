3c497b2ea63341d7fa39cbbcd8d0a7803d908caa
Norman Meßtorff <normes@normes.org>
2012-07-31 11:13:42 +0200

    cleanup old files; improved multi-repo handling

--- src/svnmock.sh
+++ src/svnmock.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
-#set -x
+#set -e -x
+set -e
 shopt -s nullglob
 
 # default values
 REPO="$1"
-REV=$2
+REV="$2"
 REPO="${REPO}/"
-TMPDIR=${REPO}tmp
-RESULTDIR="/data/result"
+TMPDIR="${REPO}tmp"
+CLEANUP="true"
+RESULTDIR="/opt/svnmock/result"
+REPOSDIR="svnmock-repos"
 MOCKCONFDIR="/etc/mock/"
+
+if [ -z "${REV}" ]; then
+        echo "Usage: $0 [SVN repository] [revision]"
+        exit 1
+fi
 
 
 #
@@ -31,7 +39,6 @@ if [ -e /etc/svnmock.conf ]; then
 fi
 
 
-
 #
 # prepare environment
 #
@@ -52,6 +59,7 @@ fi
 # check changed packages
 #
 BUILD=()
+REPOS=()
 for package in `svnlook dirs-changed --revision ${REV} "${REPO}"`; do
 	# package
 	currentpkg=${package%%/*}
@@ -62,6 +70,7 @@ for package in `svnlook dirs-changed --revision ${REV} "${REPO}"`; do
 	
 	# buildlist
 	if [ ! -z ${currentrepo} ]; then
+		# worklist
 		if [ -z "${BUILD}" ]; then
 			BUILD=("${currentpkg}/${currentrepo}")
 		fi
@@ -71,6 +80,20 @@ for package in `svnlook dirs-changed --revision ${REV} "${REPO}"`; do
 				continue;
 			else
 				BUILD=("${BUILD[@]}" "${currentpkg}/${currentrepo}")
+				break;
+			fi
+		done
+		
+		# repos
+		if [ -z "${REPOS}" ]; then
+			REPOS=("${currentrepo}")
+		fi
+		
+		for dup in ${REPOS[@]}; do
+			if [ "${dup}" == "${currentrepo}" ]; then
+				continue;
+			else
+				REPOS=("${REPOS[@]}" "${currentrepo}")
 				break;
 			fi
 		done
@@ -106,33 +129,45 @@ for target in ${BUILD[@]}; do
 	mock -v --configdir=${MOCKCONFDIR} --resultdir="${target}"/SRPMS/ --buildsrpm --spec "${target}"/SPECS/"${pkgname}".spec --source "${target}"/SOURCES/ &> ${target}/LOGS/${logprefix}mock_srpm.log
 	RET=$?
 	log "  . [DONE=${RET}] building SRPM '${target}' in ${TMPDIR}"
-	mv ${target}/LOGS/* ${RESULTDIR}/${reponame}/LOGS/
-	cp ${target}/SRPMS/*.rpm ${RESULTDIR}/${reponame}/SRPMS/
 	if [ ${RET} != 0 ]; then
 		die "Could not build '${target}'"
 	fi
 	
 	# building RPM with mock
 	log "  . building binary RPM '${target}'"
-	mock -v --configdir=${MOCKCONFDIR} --resultdir="${target}"/RPMS/  --rebuild "${target}"/SRPMS/*.src.rpm &> ${target}/LOGS/${logprefix}mock_rpm.log
+	mock -v --configdir=${MOCKCONFDIR} --resultdir=${target}/RPMS/ --rebuild ${target}/SRPMS/*.src.rpm &> ${target}/LOGS/${logprefix}mock_rpm.log
 	RET=$?
 	log "  . [DONE=${RET}] building binary RPM '${target}' in ${TMPDIR}"
-	mv ${target}/LOGS/* ${RESULTDIR}/${reponame}/LOGS/
-	mv ${target}/RPMS/*.rpm	${RESULTDIR}/${reponame}/RPMS/
 	if [ ${RET} != 0 ]; then
 		die "Could not build '${target}'"
 	fi
+	
+	# cleanup
+	if [ "${CLEANUP}" == "true" ]; then
+		log "  . removing old files"
+		rm -f ${RESULTDIR}/${reponame}/SRPMS/${pkgname}*.src.rpm ${RESULTDIR}/${reponame}/RPMS/${pkgname}*.rpm ${RESULTDIR}/${reponame}/LOGS/${pkgname}*.log
+	fi
+	mv ${target}/SRPMS/*.rpm ${RESULTDIR}/${reponame}/SRPMS/
+	mv ${target}/LOGS/* ${RESULTDIR}/${reponame}/LOGS/
+	mv ${target}/RPMS/*.rpm	${RESULTDIR}/${reponame}/RPMS/
+	rm ${RESULTDIR}/${reponame}/RPMS/*.src.rpm
 done
 
-#
-# check if we have already a .spec file
-#
-if [ ! -e ${RESULTDIR}/repos/svnmock-${reponame}.rpm ]; then
-	log "${RESULTDIR}/repos/svnmock-${reponame}.rpm not existing"
-	mkdir -p ${RESULTDIR}/repos/LOGS || die "Could not create ${RESULTDIR}/repos/LOGS"
-	logprefix="${reponame}_r${REV}_"
+for target in ${REPOS[@]}; do
+	# createrepo
+	log "  . running createrepo in ${RESULTDIR}/${target}/RPMS"
+	createrepo -v --database --update "${RESULTDIR}"/${target}/RPMS  &> ${RESULTDIR}/${target}/LOGS/createrepo.log
+	log "  . [DONE=$?] createrepo in ${RESULTDIR}/${target}/"
 
-	echo "Name:           svnmock-${reponame}
+	#
+	# check if we have already a .spec file
+	#
+	if [ ! -e ${RESULTDIR}/${REPOSDIR}/svnmock-${reponame}.spec ]; then
+		log "${RESULTDIR}/${REPOSDIR}/svnmock-${reponame}.spec not existing"
+		mkdir -p ${RESULTDIR}/${REPOSDIR}/LOGS || die "Could not create ${RESULTDIR}/${REPOSDIR}/LOGS"
+		logprefix="${reponame}_r${REV}_"
+
+		echo "Name:           svnmock-${reponame}
 Version:        1
 Release:        1%{?dist}
 Summary:        svnmock dynamic repository ${reponame}
@@ -150,7 +185,7 @@ svnmock dynamic repository ${reponame}
 %prep
 echo '[${reponame}]
 me=svnmock dynamic repository ${reponame}
-baseurl=${BASEURL}/${reponame}
+baseurl=${BASEURL}/${reponame}/RPMS
 gpgcheck=0
 enabled=1' > %{name}.repo
 
@@ -165,37 +200,38 @@ rm -rf $RPM_BUILD_ROOT
 
 %files
 %defattr(-,root,root,-)
-/etc/yum.repos.d/%{name}.repo" > ${RESULTDIR}/repos/${reponame}.spec
-	
-	log "  . building SRPM '${reponame}'"
-	mock -v --configdir=${MOCKCONFDIR} --resultdir="${RESULTDIR}"/repos/ --buildsrpm --spec "${RESULTDIR}"/repos/"${reponame}".spec --source "${RESULTDIR}/repos/" &> ${RESULTDIR}/repos/LOGS/${logprefix}mock_srpm.log
-        RET=$?	
-	log "  . [DONE=${RET}] building SRPM '${reponame}' in ${RESULTDIR}/repos"
-	if [ ${RET} != 0 ]; then
-		die "Could not build '${reponame}'.repo SRPM"
+/etc/yum.repos.d/%{name}.repo" > ${RESULTDIR}/${REPOSDIR}/svnmock-${reponame}.spec
+		
+		log "  . building SRPM '${reponame}'"
+		mock -v --configdir=${MOCKCONFDIR} --resultdir="${RESULTDIR}"/${REPOSDIR}/ --buildsrpm --spec "${RESULTDIR}"/${REPOSDIR}/svnmock-${reponame}.spec --source "${RESULTDIR}/${REPOSDIR}/" &> ${RESULTDIR}/${REPOSDIR}/LOGS/${logprefix}mock_srpm.log
+		RET=$?	
+		log "  . [DONE=${RET}] building SRPM '${reponame}' in ${RESULTDIR}/${REPOSDIR}"
+		if [ ${RET} != 0 ]; then
+			die "Could not build '${reponame}'.repo SRPM"
+		fi
+		
+		# building RPM with mock
+		log "  . building binary RPM '${reponame}'"
+		mock -v --configdir=${MOCKCONFDIR} --resultdir="${RESULTDIR}"/${REPOSDIR}/  --rebuild "${RESULTDIR}"/${REPOSDIR}/svnmock-${reponame}*.src.rpm --source "${RESULTDIR}/${REPOSDIR}/" &> ${RESULTDIR}/${REPOSDIR}/LOGS/${logprefix}mock_rpm.log
+		RET=$?
+		log "  . [DONE=${RET}] building binary RPM '${reponame}' in ${RESULTDIR}/${REPOSDIR}"
+		if [ ${RET} != 0 ]; then
+			die "Could not build '${reponame}'.repo RPM"
+		fi
+		
+		log "  . running createrepo in ${RESULTDIR}/${REPOSDIR}/"
+		createrepo -v --database --update "${RESULTDIR}"/${REPOSDIR}/  &> ${RESULTDIR}/${REPOSDIR}/LOGS/createrepo.log
+		log "  . [DONE=$?] createrepo in ${RESULTDIR}/${REPOSDIR}/"
 	fi
-	
-	# building RPM with mock
-	log "  . building binary RPM '${reponame}'"
-	mock -v --configdir=${MOCKCONFDIR} --resultdir="${RESULTDIR}"/repos/  --rebuild "${RESULTDIR}"/repos/svnmock-${reponame}*.src.rpm --source "${RESULTDIR}/repos/" &> ${RESULTDIR}/repos/LOGS/${logprefix}mock_rpm.log
-	RET=$?
-	log "  . [DONE=${RET}] building binary RPM '${reponame}' in ${RESULTDIR}/repos"
-	if [ ${RET} != 0 ]; then
-		die "Could not build '${reponame}'.repo RPM"
-	fi
-	
-	log "  . running createrepo in ${RESULTDIR}/repos/"
-	createrepo -v --database --update "${RESULTDIR}"/repos/  &> ${RESULTDIR}/repos/LOGS/createrepo.log
-	log "  . [DONE=$?] createrepo in ${RESULTDIR}/repos/"
-fi
-
-exit $?
+done
 
 
-# running post-hook (createrepo...)
+#
+# running post-hook (rsync...)
+#
 if [ -x ${RESULTDIR}/post.sh ]; then
 	log "  . running post.sh"
-	${RESULTDIR}/post.sh ${reponame} &> ${target}/LOGS/${logprefix}post.log
+	${RESULTDIR}/post.sh ${reponame} &> ${RESULTDIR}/${logprefix}post.log
 	RET=$?
 	log "  . [DONE=${RET}] post.sh"
 fi
